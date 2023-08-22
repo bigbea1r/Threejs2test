5374677af24a53037b5be9fd461159f2a6a1188f
bigbea1r <olegpanarinantizlo007@gmail.com>
2023-08-22 15:48:26 +0300

    создание шара в пространстве один-в-один со второго урока курса

--- sketch.js
+++ sketch.js
@@ -1,79 +1,69 @@
-/**
- * A basic ThreeJS cube scene.
- * @author Matt DesLauriers (@mattdesl)
- */
-
 const canvasSketch = require('canvas-sketch');
-
-// Ensure ThreeJS is in global scope for the 'examples/'
 global.THREE = require('three');
-
-// Include any additional ThreeJS examples below
 require('three/examples/js/controls/OrbitControls');
 
 const settings = {
-  // Make the loop animated
   animate: true,
-  // Get a WebGL canvas rather than 2D
   context: 'webgl',
-  // Turn on MSAA
   attributes: { antialias: true }
 };
 
 const sketch = ({ context }) => {
-  // Create a renderer
   const renderer = new THREE.WebGLRenderer({
     context
   });
 
-  // WebGL background color
-  renderer.setClearColor('#000', 1);
 
-  // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
-  camera.lookAt(new THREE.Vector3());
+  renderer.setClearColor('#333', 1);
 
-  // Setup camera controller
+
+  const camera = new THREE.PerspectiveCamera(12,window.innerWidth / window.innerHeight,.01,100);
+
+  camera.position.set(10.5,4,-3.5);
+
+  camera.setViewOffset(10, 10, -2, 0.5, 9, 9)
+
   const controls = new THREE.OrbitControls(camera, context.canvas);
-
-  // Setup your scene
   const scene = new THREE.Scene();
+  const lightHolder = new THREE.Group();
+  const aLight=new THREE.DirectionalLight(0xffffff,2);
 
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshPhysicalMaterial({
-      color: 'white',
-      roughness: 0.75,
-      flatShading: true
-    })
-  );
-  scene.add(mesh);
+  aLight.position.set(-1.5,1.7,.7);
 
-  // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f', 1));
+  lightHolder.add(aLight);
 
-  // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
-  light.position.set(2, 2, -4).multiplyScalar(1.5);
-  scene.add(light);
+  const aLight2=new THREE.DirectionalLight(0xffffff,2);
+  aLight2.position.set(-1.5,0.3,.7);
+  lightHolder.add(aLight2);
 
-  // draw each frame
+  const geometry = new THREE.IcosahedronGeometry(1.0,2)  
+  const materialIcosahedron = new THREE.MeshBasicMaterial({
+    opacity: 0,
+    transparent: true
+  });
+
+  const mesh = new THREE.Mesh(geometry,materialIcosahedron);
+
+  const parent=mesh;
+
+  const geomHide = new THREE.SphereBufferGeometry(1.0499, 64, 36);
+  const matHide=new THREE.MeshStandardMaterial({color:new THREE.Color(0x091e5a)});
+  const meshHide= new THREE.Mesh(geomHide, matHide);
+
+  scene.add(meshHide);
+  scene.add(lightHolder);
+
   return {
-    // Handle resize events here
     resize ({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
       camera.aspect = viewportWidth / viewportHeight;
       camera.updateProjectionMatrix();
     },
-    // And render events here
     render ({ time, deltaTime }) {
-      mesh.rotation.y = time * (10 * Math.PI / 180);
-      controls.update();
+      lightHolder.quaternion.copy(camera.quaternion);
       renderer.render(scene, camera);
     },
-    // Dispose of WebGL context (optional)
     unload () {
       renderer.dispose();
     }
